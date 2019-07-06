6dc000afdeda929beaad1b755ebda67728c75a2d
Chris Kong <cxkong@ucsc.edu>
2019-07-06 16:49:58 -0700

    added gameplay controller

---
+++ DescendIntoDarkness/Source/DescendIntoDarkness/GameplayController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GameplayController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESCENDINTODARKNESS_API AGameplayController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
