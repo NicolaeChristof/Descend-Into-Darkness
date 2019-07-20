250e26b33161aaf60b37e668b7e6b3186ace13dc
Chris Kong <cxkong@ucsc.edu>
2019-07-20 16:20:04 -0700

    added crafting datatable

--- DescendIntoDarkness/Source/DescendIntoDarkness/Resource.h
+++ DescendIntoDarkness/Source/DescendIntoDarkness/Resource.h
@@ -16,6 +16,7 @@ USTRUCT(BlueprintType)
 struct FResource : public FTableRowBase
 {
 	GENERATED_BODY()
+
 public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
@@ -31,25 +32,33 @@ public:
 	UTexture2D* ResourceImage;
 };
 
-/**
+USTRUCT(BlueprintType)
+struct FCraftable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Craftable)
+	FString CraftableName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Craftable)
+	FName CraftableID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Craftable)
+	int32 CraftableQuantity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Craftable)
+	UTexture2D* CraftableImage;
+
+	UPROPERTY(EditAnywhere, Category = Craftable)
+	FResource Recipe[4];
+
+};
+
+
 UCLASS(BlueprintType)
-class DESCENDINTODARKNESS_API UResource : public UObject
+class DESCENDINTODARKNESS_API UFResource : public UObject
 {
 	GENERATED_BODY()
 	
-	public:
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
-	FString ResourceName;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
-	FName ResourceID;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
-	int32 ResourceQuantity;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
-	UTexture2D* ResourceImage;
-
 };
-*/
